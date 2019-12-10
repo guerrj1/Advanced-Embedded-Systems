c0df10c3c52542fabb3e58a85ce5f6e01eeb6733
guerrj1 <47646951+guerrj1@users.noreply.github.com>
2019-12-10 00:07:28 -0800

    Add files via upload

---
+++ TIVAC/Lab_8/Lab8/Debug/softssi.d
@@ -0,0 +1,31 @@
+# FIXED
+
+softssi.obj: ../softssi.c
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+softssi.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+softssi.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+softssi.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+softssi.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h
+softssi.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom_map.h
+softssi.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/softssi.h
+
+../softssi.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/softssi.h: 
---
+++ TIVAC/Lab_8/Lab8/Debug/spi_flash.d
@@ -0,0 +1,37 @@
+# FIXED
+
+spi_flash.obj: ../spi_flash.c
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+spi_flash.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_ssi.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_udma.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom_map.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/ssi.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/udma.h
+spi_flash.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/spi_flash.h
+
+../spi_flash.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_ssi.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_udma.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/ssi.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/udma.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/spi_flash.h: 
